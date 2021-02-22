a88155f594b33812319d3ccd76ba8812151d6578
Max Nicolai <maxvnicolai@gmail.com>
2021-02-22 12:17:37 -0800

    added UserNotFoundException class

---
+++ java/src/main/java/me/max/exceptions/UserNotFoundException.java
@@ -0,0 +1,29 @@
+package me.max.exceptions;
+
+//Custom Exception, throws when user input matches no user in db
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException() {
+		super();
+	}
+
+	public UserNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public UserNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+	public UserNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+	private static final long serialVersionUID = 724244861493651341L;
+
+}
