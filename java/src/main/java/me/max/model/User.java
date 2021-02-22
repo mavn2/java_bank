fec4fb934e30d9e625f984b37003698cab8bb50d
Max Nicolai <maxvnicolai@gmail.com>
2021-02-22 14:07:13 -0800

    added basic user class to models

---
+++ java/src/main/java/me/max/model/User.java
@@ -0,0 +1,41 @@
+package me.max.model;
+
+public class User {
+
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
