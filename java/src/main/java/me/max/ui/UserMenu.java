9a671fd711943905a0d29f194a8492af89371a00
Max Nicolai <maxvnicolai@gmail.com>
2021-03-03 15:59:45 -0800

    User menu/account request complete

--- java/src/main/java/me/max/ui/UserMenu.java
+++ java/src/main/java/me/max/ui/UserMenu.java
@@ -1,16 +1,22 @@
 package me.max.ui;
 
+import java.sql.SQLException;
+
+import me.max.exceptions.AccountCreationException;
 import me.max.model.User;
+import me.max.services.CustomerService;
 
 public class UserMenu implements Menu {
 
 	// Can hold data for user accessing app
 	private User u;
+	CustomerService customerService;
 
 	// Custom constructor takes a user object as parameter,
 	// ensures access to relevant information
 	public UserMenu(User user) {
 		u = user;
+		customerService = new CustomerService();
 	}
 
 	@Override
@@ -23,7 +29,37 @@ public class UserMenu implements Menu {
 			System.out.println("USER MENU");
 			System.out.println("===============");
 			System.out.println("1.) Go back");
-			System.out.println("2.) Request an account");
+			System.out.println("2.) Apply for account");
+
+			try {
+				choice = Integer.parseInt(Menu.sc.nextLine());
+			} catch (NumberFormatException e) {
+			}
+
+			switch (choice) {
+			case 1:
+				break;
+			case 2:
+				requestAccount();
+				break;
+			default:
+				System.out.println("Please enter a valid selection");
+			}
+		}
+		;
+	}
+
+	private void requestAccount(){
+		String accountType = getAccountType();
+		Double accountBalance = getStartingBalance();
+		
+		int choice = 0;
+		boolean selected = false;
+
+		do {
+			System.out.println("Are you sure you want to apply for a " + accountType + " account with initial balance " + accountBalance);
+			System.out.println("1.) NO, go back");
+			System.out.println("2.) YES, apply for account");
 			
 			try {
 				choice = Integer.parseInt(Menu.sc.nextLine());
@@ -31,17 +67,70 @@ public class UserMenu implements Menu {
 			}
 			
 			switch(choice) {
-			case 1: 
+			case 1:
+				selected = true;
 				break;
 			case 2:
-				requestAccount();
+				selected = true;
+				break;
+				
 			}
-		};
-
+		} while (selected == false);
+		
+		if(selected == true && choice == 2) {
+			try{
+				customerService.requestNewAccount(u, accountBalance, accountType);
+				System.out.println("Application accepted! We will contact you by phone once your account has been approved.");
+			} catch (SQLException | AccountCreationException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
-	private void requestAccount(){
-		//logic
+
+	private String getAccountType() {
+		System.out.println("Please select an Account type from the list below");
+
+		System.out.println("1.) Checking");
+		System.out.println("2.) Savings");
+		System.out.println("3.) Business");
+		
+		int choice = 0;
+		boolean selected = false;
+		String result = "";
+		
+		do {
+			try {
+					choice = Integer.parseInt(Menu.sc.nextLine());
+				} catch (NumberFormatException e) {
+				}
+			switch(choice) {
+			case 1: 
+				result = "Checking";
+				selected = true;
+				break;
+			case 2:
+				result = "Savings";
+				selected = true;
+				break;
+			case 3: 
+				result = "Business";
+				selected = true;
+				break;
+			default:
+				System.out.println("Please enter a valid selection");
+			}
+		} while (selected == false);
+		
+		return result;
+	}
+
+	private double getStartingBalance() {
+		double result = 0.0;
+
+		System.out.println("Please submit a starting balance for your account");
+		result = Double.parseDouble(Menu.sc.nextLine());
+
+		return result;
 	}
 
 }
